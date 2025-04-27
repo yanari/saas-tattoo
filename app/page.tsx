2c67056f37b3733483c7121cda61c3f302f18b04
Marcelle Yanari <yanarimy@gmail.com>
2025-04-27 20:56:05 -0300

    feat: add logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,26 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SearchIcon } from "lucide-react";
 
 export default function Home() {
-  return <Button>oiiiii</Button>;
+  return (
+    <div className="p-5">
+      <h2 className="text-xl font-bold">Hello, world</h2>
+      <p>
+        {new Intl.DateTimeFormat("en-US", {
+          weekday: "long",
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        }).format(new Date())}
+      </p>
+
+      <div className="mt-6 gap-2 flex items-center">
+        <Input placeholder="Search..." />
+        <Button>
+          <SearchIcon />
+        </Button>
+      </div>
+    </div>
+  );
 }
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,10 +1,16 @@
+import { MenuIcon } from "lucide-react";
+import { Button } from "../ui/button";
 import { Card, CardContent } from "../ui/card";
+import { Logo } from "../svgs/logo";
 
 export function Header() {
   return (
-    <Card>
-      <CardContent>
-        <span></span>
+    <Card className="rounded-none">
+      <CardContent className="flex flex-row items-center justify-between ">
+        <Logo className="fill-primary h-12 w-32" />
+        <Button size="icon" variant={"outline"}>
+          <MenuIcon />
+        </Button>
       </CardContent>
     </Card>
   );
