0344de9b06146b877d306b877d0d5aedbe8b1eff
Marcelle Yanari <yanarimy@gmail.com>
2025-04-28 15:31:11 -0300

    feat: add TattooStudioItem

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import { Schedule } from "@/components/features/schedule-item";
+import { TattooStudioItem } from "@/components/features/tattoo-studio-item";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { db } from "@/lib/prisma";
+import { TattooStudio } from "@prisma/client";
 import { SearchIcon } from "lucide-react";
 
-export default function Home() {
+export default async function Home() {
+  const tattooStudios: TattooStudio[] = await db.tattooStudio.findMany();
+  console.log({ tattooStudios });
   return (
     <div className="p-5 grid gap-6">
       <div>
@@ -30,6 +35,20 @@ export default function Home() {
           Agendamentos
         </h2>
         <Schedule />
+      </div>
+
+      <div>
+        <h2 className="uppercase text-gray-400 text-sm font-bold mb-3">
+          Recomendados
+        </h2>
+        <div className="flex gap-4">
+          {tattooStudios.map((tattooStudio) => (
+            <TattooStudioItem
+              key={tattooStudio.id}
+              tattooStudio={tattooStudio}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- components/features/tattoo-studio-item.tsx
+++ components/features/tattoo-studio-item.tsx
@@ -1,7 +1,24 @@
-export function TattooStudioItem() {
+import { TattooStudio } from "@prisma/client";
+import { Card, CardContent } from "../ui/card";
+import Image from "next/image";
+
+interface TattooStudioItemProps {
+  tattooStudio: TattooStudio;
+}
+export function TattooStudioItem({ tattooStudio }: TattooStudioItemProps) {
   return (
-    <div>
-      <h2 className="text-xl font-bold">Hello, world</h2>
-    </div>
+    <Card className="min-w-full max-w-full">
+      <CardContent className="p-0">
+        <div className="relative h-40">
+          <Image
+            src={tattooStudio.imageUrl}
+            alt={tattooStudio.name}
+            className="object-cover"
+            fill={true}
+          />
+        </div>
+        <h2 className="text-xl font-bold">{tattooStudio.name}</h2>
+      </CardContent>
+    </Card>
   );
 }
